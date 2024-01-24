a56fb11cd4c218a33903e95fa0e2389df6eedbaa
cjns <67065839+cjns@users.noreply.github.com>
2024-01-24 21:52:18 +0000

    add JS sorting and 'back to top' link.

---
+++ static/js/product.js
@@ -0,0 +1,27 @@
+document.querySelectorAll('.btt-link').forEach(function(element) {
+    element.addEventListener('click', function(e) {
+        window.scrollTo(0, 0);
+    });
+});
+
+document.getElementById('sort-selector').addEventListener('change', function() {
+    var selector = this;
+    var currentUrl = new URL(window.location);
+
+    var selectedVal = selector.value;
+    if (selectedVal !== "reset") {
+        var sortDirection = selectedVal.split("_");
+        var sort = sortDirection[0];
+        var direction = sortDirection[1];
+
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+
+        window.location.replace(currentUrl.toString());
+    } else {
+        currentUrl.searchParams.delete("sort");
+        currentUrl.searchParams.delete("direction");
+
+        window.location.replace(currentUrl.toString());
+    }
+});
