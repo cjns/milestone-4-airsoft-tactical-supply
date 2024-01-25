3bec63cf487adbb002904e0fb1abfac18b4e40d0
cjns <67065839+cjns@users.noreply.github.com>
2024-01-25 13:26:46 +0000

    Update context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def cart_contents(request):
@@ -8,6 +10,17 @@ def cart_contents(request):
     total_products = 0
     delivery = 0
     free_delivery_delta = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total_cost += quantity * product.price
+        total_products += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total_cost < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total_cost * \
