9327b51e4e3ce8424b8e7b69f08742fb36ed2d0d
cjns <67065839+cjns@users.noreply.github.com>
2024-01-27 18:47:52 +0000

    Add simple checkout

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,8 @@
 // Set your publishable key: remember to change this to your live publishable key in production
 // See your keys here: https://dashboard.stripe.com/apikeys
-let stripe_public_key = document.querySelector('#id_stripe_public_key').textContent.trim().slice(1, -1);
-let client_secret = document.querySelector('#id_client_secret').textContent.trim().slice(1, -1);
-const stripe = Stripe(stripe_public_key);
+let stripePublicKey = document.querySelector('#id_stripe_public_key').textContent.trim().slice(1, -1);
+let clientSecret = document.querySelector('#id_client_secret').textContent.trim().slice(1, -1);
+const stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
 let style = {
     base: {
@@ -19,5 +19,51 @@ let style = {
         iconColor: '#dc3545'
     }
 };
-let card = elements.create('card', {style: style});
+let card = elements.create('card', { style: style });
 card.mount('#card-element');
+
+// Deal with realtime validation errors on card element
+card.addEventListener('change', function (event) {
+    let errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        let html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit - Stripe documentation / adopted from boutique ado
+let form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true });
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function (result) {
+        if (result.error) {
+            let errorDiv = document.getElementById('card-errors');
+            let html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false });
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
