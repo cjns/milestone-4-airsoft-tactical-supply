cadd103d6ac86e959f5d313d2ebad0b928a148c9
cjns <67065839+cjns@users.noreply.github.com>
2024-01-29 16:00:21 +0000

    Add confirmation emails

--- airsoft_tactical_supply/settings.py
+++ airsoft_tactical_supply/settings.py
@@ -181,3 +181,4 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_KEY = os.getenv('STRIPE_WH_KEY', '')
+DEFAULT_FROM_EMAIL = 'info@airsofttacticalsupply.com'
